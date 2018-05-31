56c4da5793f9bd48dc713f458f56d338a8f02bd6
vT <dngwu9891@gmail.com>
2018-05-31 16:00:52 -0400

    minor changes

--- controllers/byId.js
+++ controllers/byId.js
@@ -2,9 +2,9 @@
 let db_service = require('../utils/db_service')
 
 function geobyid(bp_id) {
-  return new Promise(function (resolve, reject) {
-    let sql = 
-      `select 
+    return new Promise(function (resolve, reject) {
+        let sql =
+        `select 
         id, 
         ST_ASGeoJSON(ST_transform(geom,4326)) as geoPoint, 
         "CONAME",
@@ -17,23 +17,22 @@ function geobyid(bp_id) {
         "BE_Payroll_Expense_Range",
         "BE_Payroll_Expense_Description" 
         from businesses_2014  
-        where id = ${bp_id};
-      `
+        where id = ${bp_id};`
 
-      db_service.runQuery(sql, [], (err,data) => {
-          if (err) reject(err)
-          resolve(data.rows)
-      })
-  });
+        db_service.runQuery(sql, [], (err, data) => {
+            if (err) reject(err)
+            resolve(data.rows)
+        })
+    });
 }
 
-const requestGeoById = function( request, response ) {
-    if(!request.params.id) {
+const requestGeoById = function (request, response) {
+    if (!request.params.id) {
         response.status(400)
-        .json({
-          status: 'Error',
-          responseText: 'No id'
-        })
+            .json({
+                status: 'Error',
+                responseText: 'No id'
+            })
     }
 
     geobyid(request.params.id)
@@ -42,10 +41,10 @@ const requestGeoById = function( request, response ) {
         })
         .then(data => {
             response.status(200)
-            .json({
-              data: data,
-            })
+                .json({
+                    data: data,
+                })
         })
-    
+
 }
 module.exports = requestGeoById
