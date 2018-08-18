85a676b42d09bce6c92690c3c00b5eca4c0e7d67
David <davidng9891@gmail.com>
2018-08-18 11:06:36 -0400

    Added bootstrap + minor changes

--- public/js/dataTableLoad.js
+++ public/js/dataTableLoad.js
@@ -1,8 +1,6 @@
 
 //Creates a Datatable with the information in data
 function loadTables(data){
-	//TODO: Check if exist if not clear
-	
 	// render the table(s)
 	var table = $('#table_id').DataTable({
 		data: data,
