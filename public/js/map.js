f3633326ec3784ebe8c1fbf3298620a11146a3e0
David <davidng9891@gmail.com>
2018-09-06 04:43:12 -0400

    Added tooltip to display radius

--- public/js/map.js
+++ public/js/map.js
@@ -1,18 +1,21 @@
+//File for the initialization of the leaflet Map and Event listeners
 //Setup Leaflet Map
 var markerList = []; //contains all the points from query
 //markers will contain the markers for the plugin markerClusterGroup
-var markers = L.markerClusterGroup({    
+var markers = L.markerClusterGroup({
     spiderfyOnMaxZoom: false,
     disableClusteringAtZoom: 20,
     chunkedLoading: true,
     chunkProgress: updateProgressBar
 });
-var usrMarkers = [];    //contains all the marker drawn by user
+var usrMarkers = []; //contains all the marker drawn by user
 var table;
 var lat, lon;
 // var redoBuffer = [];
 
-var mymap = L.map('mapid',{editable: true}).setView([40.755, -74.00], 13);
+var mymap = L.map('mapid', {
+    editable: true
+}).setView([40.755, -74.00], 13);
 var drawnItems = new L.FeatureGroup().addTo(mymap);
 L.tileLayer(
     'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
@@ -36,7 +39,7 @@ L.EditControl = L.Control.extend({
     },
     onAdd: function (map) {
         var container = '';
-        switch (this.options.type){
+        switch (this.options.type) {
             case 'draw':
                 container = L.DomUtil.create('div', 'leaflet-control leaflet-bar');
                 var link = L.DomUtil.create('a', '', container);
@@ -44,19 +47,22 @@ L.EditControl = L.Control.extend({
                 link.title = 'Create a new ' + this.options.kind;
                 link.innerHTML = this.options.html;
                 L.DomEvent.on(link, 'click', L.DomEvent.stop)
-                .on(link, 'click', function () {
-                    window.LAYER = this.options.callback.call(map.editTools);
-                }, this);
+                    .on(link, 'click', function () {
+                        window.LAYER = this.options.callback.call(map.editTools);
+                    }, this);
                 break;
             case 'query':
+                //Defines the query button
                 container = L.DomUtil.create('div', 'leaflet-control leaflet-bar');
                 var link = L.DomUtil.create('a', 'leaflet-control-queryBtn', container);
                 link.style = 'display:none;'
                 link.href = '#';
                 link.title = 'Query the drawing';
                 link.innerHTML = this.options.html;
-                L.DomEvent.on(link, 'click', L.DomEvent.stop)
-                .on(link, 'click', loadDrawingEstablishments, this);
+                L.DomEvent.on(link, 'click', L.DomEvent.stop);
+
+                    // L.DomEvent.on(link, 'click', L.DomEvent.stop)
+                    // .on(link, 'click', loadDrawingEstablishments, this);
                 break;
             default:
                 var container = L.DomUtil.create('div', 'leaflet-control leaflet-bar');
@@ -130,8 +136,8 @@ L.EmptyControl = L.EditControl.extend({
 mymap.addControl(new L.NewRectangleControl());
 mymap.addControl(new L.NewCircleControl());
 mymap.addControl(new L.NewMarkerControl());
-mymap.addControl(new L.EmptyControl());     //space
-mymap.addControl(new L.EmptyControl());     //space
+mymap.addControl(new L.EmptyControl()); //space
+mymap.addControl(new L.EmptyControl()); //space
 mymap.addControl(new L.NewQueryControl());
 //---
 // END MAP CONTROL TOOLS
@@ -140,56 +146,99 @@ mymap.addControl(new L.NewQueryControl());
 //---
 // MAP EVENT LISTENERS
 //---
-// mymap.on('editable:drawing:end', (e)=>{
-//     // console.log('end');
-//     // usrMarkers.push(e.layer);
-//     // drawnItems.addLayer(e.layer);
-//     // $('.leaflet-control-queryBtn').css('display','block');
-//     // drawnItems.clearLayers();
-// });
-mymap.on('editable:drawing:commit', (e)=>{
-    // console.log('commit');
+//Helper function to get drawing type.
+function drawingType(layer) {
+    if (layer instanceof L.Marker) return 'marker';
+    if (layer instanceof L.Circle) return 'circle';
+    if (layer instanceof L.Rectangle) return 'rectangle';
+    if (layer instanceof L.Polygon) return 'polygon';
+}
+
+var tooltip = L.DomUtil.get('tooltip');
+function queryDrawing(){
+    loadDrawingEstablishments();
+    // Clear tooltip but we don't want to get rid of the listeners for edits.
+    tooltip.style.display = 'none';
+}
+// Converts radius to miles and display it in the #tooltip div.
+function printRadius(e){
+    let radius = e.layer.getRadius() * 0.00062137;
+    radius = radius.toFixed(4) + 'mi';
+    tooltip.innerHTML = radius;
+    tooltip.style.display = 'block';
+}
+
+function addTooltip(e) {
+    removeTooltip(); //Get rid of old tooltip
+    if (e.layer instanceof L.Circle){
+        mymap.on('editable:drawing:move', printRadius); // To print radius
+        L.DomEvent.on(document, 'mousemove', moveTooltip); // To update div position
+    }
+}
+
+function removeTooltip() {
+    tooltip.innerHTML = '';
+    tooltip.style.display = 'none';
+    L.DomEvent.off(document, 'mousemove', moveTooltip);
+    mymap.off('editable:drawing:move', printRadius);
+}
+
+function moveTooltip(e) {
+    tooltip.style.left = e.clientX + 20 + 'px';
+    tooltip.style.top = e.clientY - 10 + 'px';
+}
+
+function addUsrMarker(e) {
+    // On drawing commit, push drawing
     usrMarkers.push(e.layer);
     drawnItems.addLayer(e.layer);
-    $('.leaflet-control-queryBtn').css('display','block');
+    $('.leaflet-control-queryBtn').css('display', 'block'); // Display the query button
+    $('.leaflet-control-queryBtn').on('click', queryDrawing);   // QUERY BUTTON LISTENER
     // drawnItems.clearLayers();
-});
+}
 
-mymap.on('editable:drawing:start', (e)=>{
-    // console.log('start'); 
+function clearUsrMarker(e) {
+    // On drawing start, clear prev marker and add tooltip.
     usrMarkers.pop();
     drawnItems.clearLayers();
-    $('.leaflet-control-queryBtn').css('display','none');
-});
+    $('.leaflet-control-queryBtn').css('display', 'none'); // hide btn so no meaningless query request
+    $('.leaflet-control-queryBtn').off('click');
+    addTooltip(e);
+}
+
+mymap.on('editable:drawing:start', clearUsrMarker);
+mymap.on('editable:drawing:commit', addUsrMarker);
 
 //Event listeners key down during drawing
-var onKeyDown = function(e) {
+var onKeyDown = function (e) {
     //ESC button to stop drawing.
     if (e.keyCode == 27) {
         if (!this.editTools._drawingEditor) return;
-        this.editTools._drawingEditor.pop();
+        let test = this.editTools._drawingEditor.pop();
+        // console.log(test);
+        this.editTools._drawingEditor.disable();
         //drawnItems.clearLayers();
         this.editTools.stopDrawing();
     }
     //UNDO button CTRL + Z.
     if (e.keyCode === 90 && e.ctrlKey) {
         if (!this.editTools._drawingEditor) {
-            //Not drawing. Remove last feature.
-            if(usrMarkers.length > 0){
+            //IF not drawing. Remove last feature.
+            if (usrMarkers.length > 0) {
                 //usually 1 drawing
                 usrMarkers.pop();
                 drawnItems.clearLayers();
-                $('.leaflet-control-queryBtn').css('display','none');
+                $('.leaflet-control-queryBtn').css('display', 'none');
+                removeTooltip();
             }
             return;
         }
         latlng = this.editTools._drawingEditor.pop();
-        if(!latlng) this.editTools.stopDrawing();
+        if (!latlng) this.editTools.stopDrawing();
     }
 };
 L.DomEvent.addListener(document, 'keydown', onKeyDown, mymap);
+
 //---
 // END MAP EVENT LISTENER
 //---
-
-// TODO: When drawing circle display radius
