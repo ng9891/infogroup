3e18a99ef9d2e7a54864012b6605fe2b3bfdc82b
David <davidng9891@gmail.com>
2018-12-07 20:35:56 -0500

    Parameterized insert query

--- controllers/editing/editBusiness.js
+++ controllers/editing/editBusiness.js
@@ -3,8 +3,8 @@ let db_service = require('../../utils/db_service');
 
 function editBusiness(id, form) {
     return new Promise(function (resolve, reject) {
-        let sql = build_query(id, form);
-        db_service.runQuery(sql, [], (err, data) => {
+        let [sql, values]= build_query(id, form);
+        db_service.runQuery(sql, values, (err, data) => {
             if (err) return reject(err.stack);
             resolve('success');
         });
@@ -12,15 +12,14 @@ function editBusiness(id, form) {
 }
 
 const editBusinessRequest = function (request, response) {
-    if (!request.params.bus_id || isNaN(request.params.bus_id)) {
+    if (!request.params.bus_id || isNaN(parseInt(request.params.bus_id, 10))) {
         return response.status(400)
             .json({
                 status: 'Error',
                 responseText: 'No Business specified or is not correct'
             });
     }
-    // CHECKING FOR INJECTIONS
-
+    
     editBusiness(request.params.bus_id, request.body)
         .then(data => {
             return response.status(200)
@@ -36,34 +35,24 @@ const editBusinessRequest = function (request, response) {
                 });
         });
 }
-
 module.exports = editBusinessRequest;
 
-//Key names have to match with table column name in DB
+// Key names have to match with table column name in DB
 function build_query(id, form) {
     let sql  = '';
     let now_string = "(now() at time zone 'utc')";
-    let insert_str = ''; //Contains the string for the INSERT in the sql. Keys.
-    let value_str = ''; //Contains the string for the VALUES in the sql
+    let insert_str = ''; // Contains the string for the INSERT in the sql. Keys.
+    let value_str = ''; // Contains the Parameterized string for the VALUES in the sql
+    let values = []; // Array of values to insert into table
     
     let key_arr = Object.keys(form);
-    // key_arr = key_arr.filter().map((k,i) => {
-        // if (i === key_arr.length-1) {
-        //     // last one
-        //     insert_str += '"'+k+'"';
-        //     value_str += form[k];
-        // } else {
-        //     // not last one
-        //     insert_str += '"'+k+'",'; //keys
-        //     value_str += form[k]+',';
-        // }
-    // });
-
+    let j = 1;
     for(let i=0; i < key_arr.length; i++){
         let k = key_arr[i];
         if (form[k]){
             insert_str += '"'+k+'",';
-            value_str += form[k]+',';
+            value_str += '$' + (j++) +',';
+            values.push(form[k]);
         }
     }
     insert_str = insert_str.slice(0, -1);
@@ -75,13 +64,7 @@ function build_query(id, form) {
         VALUES (${id},${now_string},0,0,'test-admin',${value_str});
         `;
 
-    // console.log(form);
-    // console.log(sql);
-    // return `
-    // INSERT INTO business_audit(business_id,created_at,record_status,status)
-    // VALUES (${id},${now_string},0,0);
-    // `;
-    return sql;
+    return [sql, values];
 }
 
 //list
